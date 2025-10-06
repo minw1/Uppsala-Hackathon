a8077686e6ac92c181352465994de67e0d1b2c25
BlueJ1 <l.august@posteo.net>
2025-10-06 16:51:21 +0200

    expand ask_question.py to enhance language detection and update system prompt for Uppsala municipal support

--- ask_question.py
+++ ask_question.py
@@ -1,7 +1,7 @@
 import dotenv
 from langchain.chat_models import init_chat_model
 from langchain_chroma import Chroma
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.tools import tool
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langgraph.graph import END
@@ -13,19 +13,20 @@ from langgraph.graph import StateGraph
 from langdetect import detect, DetectorFactory
 from pathlib import Path
 import json
-#merge fixed?
 
 DetectorFactory.seed = 0  # make detection deterministic
 
 dotenv.load_dotenv()
 
 system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise. The context documents are in Swedish, but it is essential that you answer in the same "
-    "language that the question is in."
+    "You are a question-answering assistant for the municipality of Uppsala. "
+    "You are providing useful information and points of contact for people that want to make use of the municipal "
+    "support system in Uppsala. Individuals should be able to search for and find relevant "
+    "information based on their needs and also receive guidance on what the next step might be, e.g., "
+    "applying for a specific measure or contacting a person/organization to find out more. If you don't know answer "
+    "answer, say that you don't know. Use three sentences maximum and keep the  answer concise. The context documents "
+    "are in Swedish, but it is essential that you answer in the same language that the question is in. If the "
+    "question is in English, answer in English."
 )
 is_first_question = True
 
@@ -37,8 +38,7 @@ vector_store = Chroma(
     persist_directory="./chroma_hackathon_db",  # Where to save data locally, remove if not necessary
 )
 
-
-LANG_FILE = Path(__file__).parent / "languages.json"
+LANG_FILE = Path(__file__).parent / "config" / "languages.json"
 
 try:
     with open(LANG_FILE, "r", encoding="utf-8") as f:
@@ -46,17 +46,14 @@ try:
 except FileNotFoundError:
     LANG_NAME = {}
 
-def last_human_text(state) -> str | None:
-    for msg in reversed(state["messages"]):
-        if getattr(msg, "type", None) == "human" or getattr(msg, "role", None) == "user":
-            return getattr(msg, "content", "") or (msg.get("content") if isinstance(msg, dict) else "")
-    return None
 
-def detect_lang_code(text: str) -> str:
-    try:
-        return detect(text)
-    except Exception:
-        return "en"  # TODO: swap this to swedish
+@tool
+def determine_language(state) -> str:
+    """Determine the language of the last human/user message."""
+    for msg in reversed(state["messages"]):
+        if getattr(msg, "type") == "human" or getattr(msg, "role") == "user":
+            return LANG_NAME[detect(msg.content)]
+    return "Swedish"
 
 
 graph_builder = StateGraph(MessagesState)
@@ -73,10 +70,10 @@ def retrieve(query: str):
     return serialized, retrieved_docs
 
 
-llm_with_tools = llm.bind_tools([retrieve])
+llm_with_tools = llm.bind_tools([retrieve, determine_language])
 
 
-# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+# Generate an AIMessage that may include a tool-call to be sent.
 def generate(state: MessagesState):
     """Generate tool call for retrieval or respond."""
     response = llm_with_tools.invoke(state["messages"])
@@ -84,8 +81,7 @@ def generate(state: MessagesState):
     return {"messages": [response]}
 
 
-# Step 2: Execute the retrieval.
-tools = ToolNode([retrieve])
+tools = ToolNode([determine_language, retrieve])
 
 graph_builder.add_node(generate)
 graph_builder.add_node(tools)
@@ -106,9 +102,20 @@ graph = graph_builder.compile(checkpointer=memory)
 config = {"configurable": {"thread_id": "adz123"}}
 
 
-def ask_question(question):
+def ask_question(question: str):
+    question_lang = detect(question)
+    print(f'Please answer in {LANG_NAME[question_lang] if question_lang in LANG_NAME else "Swedish"}.')
+    human_message = HumanMessage(question + f'\nPlease answer in {LANG_NAME[question_lang] if question_lang in LANG_NAME else "Swedish"}.')
+    # noinspection PyTypeChecker
     response = graph.invoke(
-        {"messages": [SystemMessage(system_prompt), HumanMessage(question)] if is_first_question else [
-            HumanMessage(question)]},
+        {"messages": [SystemMessage(system_prompt),
+                      AIMessage("Hej, I'm your Support Guide Chatbot from the municipality of Uppsala! I can help you "
+                                "find information about municipal support and servies - like activities, contact "
+                                "persons or applications. Vi kan också chatta på svenska!"),
+                      human_message] if is_first_question else [human_message]},
         config=config)
     return response['messages'][-1].content
+
+
+print(ask_question("ye kaise kaam krta hai"))
+print(ask_question("Pouvez-vous me donner un contact concret pour obtenir du soutien en santé mentale ?"))
